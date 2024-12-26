dd6e255c46c210ef8e846d35ecfe77b130e2aa39
SyedWahib <syedwahibwali7@gmail.com>
2024-12-26 22:56:18 +0000

    Succesfully finished the most basic version of the app.
    Will hopefully come back to this later and upgrade more to include animations depending on the weather.

--- WeatherApp.py
+++ WeatherApp.py
@@ -3,21 +3,6 @@ import requests
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QDesktopWidget, QLineEdit, QPushButton
 from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtCore import Qt
-
-"""api_key= os.getenv("ApiKeyForWeatherApp")
-
-city_name=input("Enter the name of the city you want to find the weather for: ")
-
-url=f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric"
-
-data= requests.get(url)"""
-
-#print(data.status_code)
-
-#print(data.json()['weather'][0]['description'])
-
-#print(data.json())
 
 
 class MainWindow(QMainWindow):
@@ -27,12 +12,15 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Weather Report")
         self.setGeometry(700, 300, 500, 500)
         self.setWindowIcon(QIcon(r"Assets\Icon.png"))
-        self.setStyleSheet("""background: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0,
-                            stop:0 hsl(231, 79%, 53%), stop:1 hsl(231, 100%, 40%));""")
+        self.setStyleSheet("""
+                            background: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0,
+                            stop:0 hsl(231, 79%, 53%), stop:1 hsl(231, 100%, 40%));
+                           """)
 
         self.center()
 
         self.city_name= QLineEdit(self)
+        self.city_name.setPlaceholderText("Enter the city name for weather details")
         self.submit_button=QPushButton("Submit", self)
         self.submit_button.setObjectName("submit_button")
 
@@ -42,24 +30,72 @@ class MainWindow(QMainWindow):
     def initUI(self):
 
         self.city_name.setFont(QFont("Inter", 10))
-        self.city_name.setGeometry(10, 20, 200, 40)
+        self.city_name.setGeometry(10, 20, 400, 40)
         self.city_name.setStyleSheet("""
-    color: black;
-    font-weight: bold;
-    background-color: white;
-    border: 2px solid hsl(231, 100%, 20%);
-    border-radius: 5px;
-    padding: 5px;
-""")
-        self.submit_button.setGeometry(210, 20, 75, 40)
+                                        color: black;
+                                        font-weight: bold;
+                                        background-color: white;
+                                        border: 2px solid hsl(231, 100%, 20%);
+                                        border-radius: 5px;
+                                        padding: 5px;
+                                    """)
+        self.submit_button.setGeometry(410, 20, 75, 40)
         self.submit_button.setFont(QFont("Helvetica", 7))
         self.submit_button.setStyleSheet("""
-            color: black;
-            font-weight: bold;
-            border: 2px solid #01168c;
-            border-radius: 5px;
-            padding: 5px;
-            background-color: hsl(231, 98%, 56%);""")
+                                        QPushButton#submit_button{
+                                        color: black;
+                                        font-weight: bold;
+                                        border: 2px solid #01168c;
+                                        border-radius: 5px;
+                                        padding: 5px;
+                                        background-color: hsl(231, 98%, 56%);}
+                                                                    
+                                        QPushButton#submit_button:hover{background-color: hsl(231, 98%, 76%);}
+                                                                    
+                                        QPushButton#submit_button:pressed{background-color: white;
+                                                                    color: grey;}
+                                         """)
+        
+        self.submit_button.clicked.connect(self.weatherFinder)
+
+    def weatherFinder(self):
+
+        self.city=self.city_name.text()
+
+        api_key= os.getenv("ApiKeyForWeatherApp")
+        url=f"https://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={api_key}&units=metric"
+        data= requests.get(url)
+        weather_details= data.json()
+        weather_main_temperature= QLabel(f"{weather_details["main"]["temp"]}°c", self)
+        weather_main_temperature.setGeometry(10, 160, 400, 40)
+        weather_main_temperature.setFont(QFont("Inter", 12))
+        weather_main_temperature.setStyleSheet("""
+                                        color: white;
+                                        font-weight: bold;
+                                        background-color: hsl(231, 98%, 40%);
+                                        border-radius: 5px;
+                                        padding: 5px;
+                                        """)
+
+        weather_main_description= QLabel(f"{weather_details["weather"][0]["main"]}", self)
+        weather_main_description.setGeometry(10, 80, 400, 40)
+        weather_main_description.setFont(QFont("Inter", 12))
+        weather_main_description.setStyleSheet("""
+                                        color: white;
+                                        font-weight: bold;
+                                        background-color: hsl(231, 98%, 40%);
+                                        border-radius: 5px;
+                                        padding: 5px;
+                                        """)
+        
+        weather_main_temperature.show()
+        weather_main_description.show()
+        
+        #print(data.status_code)
+
+        #print(data.json()['weather'][0]['description'])
+
+        #print(data.json())
 
     def center(self):
         """This method centers the window on the user's monitor"""
